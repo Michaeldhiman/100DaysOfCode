21caefc4172d329c3b6e6a1a5016dfe646e2d07f
Michael Dhiman <134770731+Michaeldhiman@users.noreply.github.com>
2024-02-03 20:35:42 +0530

    Create Square Root.c

---
+++ Day30/Square Root.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int test_case;
+    printf("Enter the Test case:\n");
+    scanf("%d",&test_case);
+    while(test_case--){
+        long long int number;
+        printf("Enter the Number:\n");
+        scanf("%lld",&number);
+        long long int sqrt=0;
+        long long int i=1;
+        while(i*i<=number){
+            sqrt=i;
+            i++;
+        }
+        printf("The Square Root of %lld is %lld",number,sqrt);
+    }
+    return 0;
+}
