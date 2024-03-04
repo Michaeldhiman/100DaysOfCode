23ed867723c63eef95fb84dd9a01d1a1ba2567f1
Michael Dhiman <134770731+Michaeldhiman@users.noreply.github.com>
2024-03-04 19:49:15 +0530

    Update Prime Reversal.c

--- Day60/Prime Reversal.c
+++ Day60/Prime Reversal.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+        int test_Case;
+        scanf("%d",&test_Case);
+        while(test_Case--){
+            int length;
+            scanf("%d",&length);
+            char str1[length];
+            char str2[length];
+            scanf("%s %s",str1,str2);
+            int one_count1=0;
+            int one_count2=0;
+            for(int i=0;i<length;i++){
+                if(str1[i]=='1') one_count1++;
+                if(str2[i]=='1') one_count2++;
+            }
+            if(one_count1==one_count2){
+                printf("yes\n");
+            }else{
+                printf("no\n");
+            }
+        }
+        return 0;
+}
+
 
